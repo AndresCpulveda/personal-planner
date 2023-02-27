629908f0b2f6e939bcdade0f4a792bd0144cfba0
Andres Sepulveda <andresfelipesepulveda@live.com>
2023-02-27 11:24:45 -0500

    changed styles of checkbox and add buttons

--- frontend/src/components/AddTask.jsx
+++ frontend/src/components/AddTask.jsx
@@ -21,10 +21,11 @@ function AddTask() {
 
 
   return (
+    <form>
     <ul className='grid grid-cols-10'>
       <li className='col-span-1 flex justify-center border border-white px-4 text-white p-4' >
-        <span onClick={() => {addNewTask()}}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-green-500 mr-2 cursor-pointer hover:text-green-700">
+        <span  onClick={() => {addNewTask()}}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-green-700 mr-2 cursor-pointer hover:text-green-500">
             <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
         </span>
@@ -46,7 +47,7 @@ function AddTask() {
           onChange={e => setDue(e.target.value)}
         ></input></li>
       <li className='col-span-1 flex border border-white'>
-        <select defaultValue={"empty"} className='bg-transparent w-full text-white px-2 ' onChange={e => setPriority(e.target.value)}>
+        <select required defaultValue={"empty"} className='bg-transparent w-full text-white px-2 ' onChange={e => setPriority(e.target.value)}>
           <option value="empty"></option>
           <option value="low">Low</option>
           <option value="medium">Medium</option>
@@ -63,6 +64,7 @@ function AddTask() {
         </div>
       </li>
     </ul>
+    </form>
 
   )
 }
--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -20,7 +20,11 @@ function Task({task}) {
             </span>
           </li> :
           <li className='col-span-1 flex items-center justify-center border border-white p-4'>
-            <input type='checkbox' onChange={() => addToCompleted(task)} className=''></input>
+            <span onClick={() => addToCompleted(task)}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-500 mr-2 cursor-pointer active:text-green-500">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+            </span>
           </li>
         }
         <li className='col-span-3 flex items-center border border-white text-white px-2'>{name}</li>
