4833e4c2c869897380ff101de794a3f59c6edb66
Andres Sepulveda <andresfelipesepulveda@live.com>
2023-03-30 11:45:27 -0500

    changes

--- src/helpers/helpers.jsx
+++ src/helpers/helpers.jsx
@@ -15,6 +15,28 @@ export function timeDeFormatter(timer) {
   return hours * 3600 + minutes * 60 + seconds
 }
 
+export function dateFormatter(date) {
+  const months = {
+    '01': 'January',
+    '02': 'February',
+    '03': 'March',
+    '04': 'April',
+    '05': 'May',
+    '06': 'June',
+    '07': 'July',
+    '08': 'August',
+    '09': 'September',
+    '10': 'October',
+    '11': 'November',
+    '12': 'December',
+  }
+
+  const splitDate = date.split('T')[0];
+  const dateArray = splitDate.split('-');
+  const formattedDate = `${dateArray[2]} ${months[dateArray[1]]} ${dateArray[0]}`
+  return formattedDate
+}
+
 export function sortPriority(list, boolean) { //OPTIMIZAR
   const sorted = list.sort((a, b) => {
     const priorityOrder = {High: 1, Medium: 2, Low: 3}
