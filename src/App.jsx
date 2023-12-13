7c65aa3ed1d598d19d7c45db091b73a78017efa5
Andres Sepulveda <andresfelipesepulveda@live.com>
2023-12-12 21:14:34 -0500

    Delete un used files of tasks provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import {Route, Routes, BrowserRouter} from 'react-router-dom'
 
 import MainLayout from './Layouts/MainLayout'
 import TodayTasks from './pages/TodayTasks'
-import { TasksProvider } from './context/tasksProvider'
 import AllTasksPage from './pages/AllTasksPage'
-import AddTask from './components/AddTask'
 import ReportsPage from './pages/ReportsPage'
 
 function App() {
@@ -13,16 +10,14 @@ function App() {
   return (
     <>
       <BrowserRouter>
-          <TasksProvider>
-            <Routes>
-              <Route path='/' element={<MainLayout />}>
-                <Route index element={<TodayTasks />}/>
-                <Route path='/tasks' element={<AllTasksPage />}/>
-                {/* <Route path='/add' element={<AddTask />}/> */}
-                <Route path='/reports' element={<ReportsPage />}/>
-              </Route>
-            </Routes>
-          </TasksProvider>
+          <Routes>
+            <Route path='/' element={<MainLayout />}>
+              <Route index element={<TodayTasks />}/>
+              <Route path='/tasks' element={<AllTasksPage />}/>
+              {/* <Route path='/add' element={<AddTask />}/> */}
+              <Route path='/reports' element={<ReportsPage />}/>
+            </Route>
+          </Routes>
       </BrowserRouter>
     </>
   )
