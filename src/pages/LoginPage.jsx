117e4c14728cd05235347194865ba67c2fbd4876
Andres Sepulveda <andresfelipesepulveda@live.com>
2024-01-16 20:25:40 -0500

    add authStatChanged listener to the login page to obtain user if logged in and redirect to admin panel

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {Link, useNavigate} from "react-router-dom"
 import { signInWithGooglePopup } from '../utils/firebase/firebase.utils'
-
+import { auth } from '../utils/firebase/firebase.utils'
 const LoginPage = () => {
+  const navigate = useNavigate()
 
   const handleSignInWithGooglePopup = async() => {
     const user = await signInWithGooglePopup()
-    console.log(user);
   }
+
+  console.log(auth);
+
+  useEffect(() => {
+    const unsuscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        //User is signed in // save to redux
+        
+        setTimeout(() => {
+          navigate('/')//Se redirecciona a la pagina de admin
+        }, 2000);
+      } else {
+        //User is signed out
+      }
+    })
+  }, [])
 
   return (
     <>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,7 +19,7 @@ const app = initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
 
-const auth = getAuth();
+export const auth = getAuth();
 
 export const signInWithGooglePopup = async () => {
   try {
@@ -48,4 +48,6 @@ export const signOutUser = () => {
     // An error happened.
   });
 }
+
+export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback)
 
